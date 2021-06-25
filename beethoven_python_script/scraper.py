8f03b9710b50620860c8983e6e1f6a816ffdb28e
Daria <andronikova.daria@gmail.com>
2021-06-25 20:00:24 +0300

    data was preprocessed in jupyter

--- beethoven_python_script/scraper.py
+++ beethoven_python_script/scraper.py
@@ -35,38 +35,24 @@ gdp_table_data = gdp_table.find_all("tr")
 
 
 # info = []
-with open("scrap_output.csv", "w", encoding="utf-8") as text_file:
-    text_file.write('Op., WoO, Hess, Biamonti, Title, Key, Date, Scoring, Genre, Comments\n')
+with open("../scrap_output.csv", "w", encoding="utf-8") as text_file:
+    text_file.write('Op.,WoO,Hess,Biamonti,Title,Key,Date,Scoring,Genre,Comments\n')
     for tr in gdp_table_data:
     #     # print('-----')
+    #     print()
+    #     print(tr)
         line=""
         for td in tr.find_all("td"):
-            if ('—' in td.text) or (len(td.text.strip())==0):
+
+            td_text = td.text.strip()
+            if len(td_text)==0:
                 line += ', ' + 'NaN'
             else:
-                line += ', ' + td.text.rstrip().replace(',',' ').replace('♯',' sharp').replace('♭',' flat')
+                line += ', ' + td_text.replace(',',' ').replace('♯',' sharp').replace('♭',' flat').lstrip('0')
 
-        line+= '\n'
+
+        line += '\n'
 
         # print(line)
         text_file.write(line[2:])
-        # info_1.append(td.text)
 
-    # info.append(info_1)
-
-
-# #  read only country name and number of regions and save it in json
-# json_db = {}
-# for country in info:
-#     name = country[0].replace(u'\xa0', u' ')
-#
-#     if len(country) >= 2:
-#         if len(country[1].split()) >= 1:
-#             json_db[name[1:-1]] = country[1].split()[0]
-#
-#
-# # print(json_db)
-#
-# # save json in file
-# with open ('countrie_regions.json','w') as json_file:
-#     json.dump(json_db, json_file, indent = 6)
